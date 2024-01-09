aceed90b49539d719f89c4dd61290763878b2886
Sefer Tasdemir <sefer.tasdemir@etu.univ-smb.fr>
2024-01-09 17:06:02 +0100

    xxx

--- GalaxyRush/MainWindow.xaml.cs
+++ GalaxyRush/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace GalaxyRush
@@ -21,23 +10,25 @@ namespace GalaxyRush
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        /*private void CanvasKeyIs(object sender, KeyEventArgs e)
+
+        private void CanvasKeyIs(object sender, KeyEventArgs e)
         {
             // on gère les booléens gauche et droite en fonction de l’appui de la touche
             if (e.Key == Key.Left)
             {
                 goUp = true;
             }
-        }*/
+        }
         private void GameEngine(object sender, EventArgs e)
         {
 
         }
-        //private bool goUp = false;
+        private bool goUp = false;
         // crée une nouvelle instance de la classe dispatch timer
         private DispatcherTimer dispatcherTimer = new DispatcherTimer();
     }
