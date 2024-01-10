b0c1e2a1eca705d194ad909b41de6922afbfec04
Olivier Brunel <Olivier.Brunel@etu.univ-smb.fr>
2024-01-10 08:45:59 +0100

    ghr

--- GalaxyRush/MainWindow.xaml.cs
+++ GalaxyRush/MainWindow.xaml.cs
@@ -16,19 +16,36 @@ namespace GalaxyRush
             InitializeComponent();
         }
 
-        private void CanvasKeyIs(object sender, KeyEventArgs e)
+        private void CanvasKeyIsDown(object sender, KeyEventArgs e)
+        {
+            bool truc = goUp;
+            // on gère les booléens gauche et droite en fonction de l’appui de la touche
+            if (e.Key == Key.Space && truc == true)
+            {
+                goUp = false;
+                goRight = false;
+            }
+        }
+        private void CanvasKeyIsUp(object sender, KeyEventArgs e)
         {
             // on gère les booléens gauche et droite en fonction de l’appui de la touche
-            if (e.Key == Key.Left)
+            if (e.Key == Key.Space && goUp == true)
             {
                 goUp = true;
+                goRight = true;
+            }
+            if (e.Key == Key.Space && goUp == false)
+            {
+                goUp = false;
+                goRight = false;
             }
         }
         private void GameEngine(object sender, EventArgs e)
         {
 
         }
-        private bool goUp = false;
+        private bool goUp, goRight = true;
+        private bool goDown = false;
         // crée une nouvelle instance de la classe dispatch timer
         private DispatcherTimer dispatcherTimer = new DispatcherTimer();
     }
