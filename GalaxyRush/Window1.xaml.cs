3e2423a310b33be2fe66f726fc1890e2649db078
Sefer Tasdemir <sefer.tasdemir@etu.univ-smb.fr>
2024-01-12 09:40:45 +0100

    commentaire les skin

--- GalaxyRush/Window1.xaml.cs
+++ GalaxyRush/Window1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -12,6 +13,7 @@ namespace GalaxyRush
     {
         private ImageBrush fondMenu = new ImageBrush();
         private ImageBrush fondMenuLogo = new ImageBrush();
+        private ImageBrush fuseeRouge = new ImageBrush();
         int skinNum = 0;
         string fuseeChoisi;
 
@@ -44,53 +46,55 @@ namespace GalaxyRush
             Tuto.Show();
         }
 
-        private void ChoisirSkin_Click(object sender, RoutedEventArgs e)
-        {
-            //increment difficulty choice on each click
-            skinNum++;
+        //private void ChoisirSkin_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //increment difficulty choice on each click
+        //    skinNum++;
 
-            switch (skinNum)
+        //    switch (skinNum)
 
-            {
-                case 1:
-                    skinFusee basiqueFusee = new fusee_vert(new DefaultFusee());
-                    fuseeChoisi = basiqueFusee.getSkin();
-                    ImageSource? skin = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
-                    fusee.Source = skin;
-                    break;
-                case 2:
-                    skinFusee basiqueFusee1 = new Fusee_missile(new DefaultFusee());
-                    fuseeChoisi = basiqueFusee1.getSkin();
-                    ImageSource? skin1 = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
-                    fusee.Source = skin1;
-                    break;
-                case 3:
-                    skinFusee basiqueFusee2 = new Fusee_violet(new DefaultFusee());
-                    fuseeChoisi = basiqueFusee2.getSkin();
-                    ImageSource? skin2 = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
-                    fusee.Source = skin2;
-                    break;
-                case 4:
-                    skinFusee basiqueFusee3 = new Fusee_rouge(new DefaultFusee());
-                    fuseeChoisi = basiqueFusee3.getSkin();
-                    ImageSource? skin3 = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
-                    fusee.Source = skin3;
-                    break;
-                case 5:
-                    skinFusee basiqueFusee4 = new Fusee_noir(new DefaultFusee());
-                    fuseeChoisi = basiqueFusee4.getSkin();
-                    ImageSource? skin4 = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
-                    fusee.Source = skin4;
-                    break;
-                default:
-                    skinFusee basicLlama2 = new DefaultFusee();
-                    fuseeChoisi = basicLlama2.getSkin();
-                    ImageSource? skin6 = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
-                    fusee.Source = skin6;
+        //    {
+        //        case 1:
+        //            skinFusee basiqueFusee = new fusee_vert(new DefaultFusee());
+        //            fuseeChoisi = basiqueFusee.getSkin();
+        //            ImageSource? skin = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
+        //            fusee.Source = skin;
+        //            break;
+        //        case 2:
+        //            skinFusee basiqueFusee1 = new Fusee_missile(new DefaultFusee());
+        //            fuseeChoisi = basiqueFusee1.getSkin();
+        //            ImageSource? skin1 = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
+        //            fusee.Source = skin1;
+        //            break;
+        //        case 3:
+        //            skinFusee basiqueFusee2 = new Fusee_violet(new DefaultFusee());
+        //            fuseeChoisi = basiqueFusee2.getSkin();
+        //            ImageSource? skin2 = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
+        //            fusee.Source = skin2;
+        //            break;
+        //        case 4:
+        //            skinFusee basiqueFusee3 = new Fusee_rouge(new DefaultFusee());
+        //            fuseeChoisi = basiqueFusee3.getSkin();
+        //            ImageSource? skin3 = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
+        //            fusee.Source = skin3;
+        //            break;
+        //        case 5:
+        //            skinFusee basiqueFusee4 = new Fusee_noir(new DefaultFusee());
+        //            fuseeChoisi = basiqueFusee4.getSkin();
+        //            ImageSource? skin4 = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
+        //            fusee.Source = skin4;
+        //            break;
+        //        default:
+        //            skinFusee basicLlama2 = new DefaultFusee();
+        //            fuseeChoisi = basicLlama2.getSkin();
+        //            ImageSource? skin6 = new ImageSourceConverter().ConvertFromString(fuseeChoisi) as ImageSource;
+        //            fusee.Source = skin6;
 
-                    skinNum = 0;
-                    break;
-            }
+        //            skinNum = 0;
+        //            break;
+        //    }
+        //}
+
         }
     }
 }
